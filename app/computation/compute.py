12c2eb18e1167e32a0a5e982d68a75af7a433d7e
WarCloud47 <zenlunatic47@gmail.com>
2019-09-22 18:12:28 +0400

    avoid file rewrite, add mpld3 and boken visualiz.

--- app/computation/compute.py
+++ app/computation/compute.py
@@ -1,5 +1,7 @@
 from numpy import exp, cos, linspace, arctan, nditer, zeros
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt, mpld3
+#import bokeh.plotting as plt
+#from bokeh.embed import components
 import os, time, glob
 
 def z_func(x, b):
@@ -14,25 +16,46 @@ def compute(A, b, w, T, resolution=500):
     #x = linspace(Xbeg, Xend, (Xend-Xbeg)/dx + 1)
     u = damped_vibrations(t, A, b, w)
     z = z_func(t, b)
-    plt.figure() # needed to avoid adding curves in plot
-    plt.plot(t, u, label ='damped_sin')
-    plt.plot(t, z, label ='arctg(x) + b', color = 'red')
-    plt.grid()
-    plt.title('A=%g, b=%g, w=%g' % (A, b, w))
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.legend()
-    #plt.ylim(-10,10)
-
-    if not os.path.isdir('app/static'):
-        os.mkdir('app/static')
-    else:
-        for filename in glob.glob(os.path.join('app/static', '*.png')):
-            os.remove(filename)
-    plotfile = os.path.join('app/static', str(time.time()) + '.png')
-    plt.savefig(plotfile)
-    plotfile = plotfile[4:]
-    return plotfile
+    fig, ax = plt.subplots(figsize=(6,4)) # needed to avoid adding curves in plot
+    ax.plot(t, u, label ='damped_sin')
+    ax.plot(t, z, label ='arctg(x) + b', color = 'red')
+    ax.grid()
+    ax.set_title('A=%g, b=%g, w=%g' % (A, b, w))
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.legend()
+    html_text = mpld3.fig_to_html(fig)
+    #plt.set_ylim(-10,10)
+    return html_text
 
 if __name__ == '__main__':
     print(compute(1, 0.1, 1, 20))
+
+
+#TOOLS = "pan, wheel_zoom, box_zoom, reset, save, box_select, lasso_select"
+#    p = plt.figure(
+#        title = 'A=%g, b=%g, w=%g' % (A, b, w),
+#        tools = TOOLS,
+#        x_axis_label ="X",
+#        y_axis_label = "Y")
+#        #y_axis_type ="log", y_range=[0.0001, 10*11]
+#        #plot_width/height, toolbar_location, h/v_symmentry, min_border, responsive, outline_line_color
+#
+#    p.line(t, u, legend ='damped_sin', line_width = 2)
+#    p.line(t, z, legend ='arctg(x) + b', line_width = 2, line_color = 'red')
+#
+#    script, div = components(p)
+#
+#    head = """
+#<link rel="stylesheet"
+#href="http://cdn.pydata.org/bokeh/release/bokeh-1.3.4.min.css"
+#type="text/css" />
+#<script type="text/javascript"
+#src="http://cdn.pydata.org/bokeh/release/bokeh-1.3.4.min.js">
+#</script>
+#<script type="text/javascript">
+#Bokeh.set_log_level("info");
+#</script>
+#"""
+#
+#    return head, script, div
